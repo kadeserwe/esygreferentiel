796c20028236bb465dbcb01f760e2c699255d210
kadeserwe <amotekades@gmail.com>
2021-07-28 14:35:40 +0000

    envoie

---
+++ src/main/java/sn/ssi/sigmap/domain/Pays.java
@@ -0,0 +1,92 @@
+package sn.ssi.sigmap.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+/**
+ * A Pays.
+ */
+@Entity
+@Table(name = "pays")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Pays implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "libelle")
+    private String libelle;
+
+    @Column(name = "codepays")
+    private String codepays;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public Pays libelle(String libelle) {
+        this.libelle = libelle;
+        return this;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getCodepays() {
+        return codepays;
+    }
+
+    public Pays codepays(String codepays) {
+        this.codepays = codepays;
+        return this;
+    }
+
+    public void setCodepays(String codepays) {
+        this.codepays = codepays;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Pays)) {
+            return false;
+        }
+        return id != null && id.equals(((Pays) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Pays{" +
+            "id=" + getId() +
+            ", libelle='" + getLibelle() + "'" +
+            ", codepays='" + getCodepays() + "'" +
+            "}";
+    }
+}
