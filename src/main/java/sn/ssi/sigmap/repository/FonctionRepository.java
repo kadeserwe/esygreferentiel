5f58dbc074c47408d5c51a82fb24db837baeeda8
chheikhlo <chheikhlo@gmail.com>
2021-07-28 17:28:10 +0000

    webService Fonction

---
+++ src/main/java/sn/ssi/sigmap/repository/FonctionRepository.java
@@ -0,0 +1,14 @@
+package sn.ssi.sigmap.repository;
+
+import sn.ssi.sigmap.domain.Fonction;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data  repository for the Fonction entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface FonctionRepository extends JpaRepository<Fonction, Long> {
+}
